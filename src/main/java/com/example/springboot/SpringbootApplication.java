0acb841822bffa98725dad349ed100e7a09bf1ee
Yong <453647824@qq.com>
2019-12-27 16:38:10 +0800

    Nacos

--- src/main/java/com/example/springboot/SpringbootApplication.java
+++ src/main/java/com/example/springboot/SpringbootApplication.java
@@ -1,5 +1,6 @@
 package com.example.springboot;
 
+import com.alibaba.nacos.spring.context.annotation.config.NacosPropertySource;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -7,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 // 在启动类中添加对mapper包扫描@MapperScan
 @MapperScan("com.example.springboot")
+@NacosPropertySource(dataId = "nacos-test", autoRefreshed = true)
 public class SpringbootApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/example/springboot/test/Test.java
+++ src/main/java/com/example/springboot/test/Test.java
@@ -1,5 +1,7 @@
 package com.example.springboot.test;
 
+import com.alibaba.nacos.api.config.annotation.NacosValue;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,5 +12,17 @@ public class Test {
     @RequestMapping(value = "/a",method = RequestMethod.GET)
     public String a(){
         return "a";
+    }
+
+    @NacosValue(value = "${useLocalCache:false}", autoRefreshed = true)
+    private boolean useLocalCache;
+
+    public void setUseLocalCache(boolean useLocalCache) {
+        this.useLocalCache = useLocalCache;
+    }
+
+    @GetMapping("getConfig.api")
+    public boolean get() {
+        return useLocalCache;
     }
 }
